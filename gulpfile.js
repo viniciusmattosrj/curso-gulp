ceadffdcfd53afcd00eb819c82d69d94703cb042
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-02-09 16:52:43 -0200

    Update : Curso Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,8 @@ var rename = require('gulp-rename');
 var cssmin = require('gulp-cssmin');
 
 gulp.task('rolo-compressor', function(){
-	gulp.src('dev/assets/css/style.css');
+	gulp.src('dev/assets/css/style.css')
+	.pipe(cssmin())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest('prod/assets/css/'));
 });
